781b3c910f9fc2aed44ceada36d2fcaac4da9bb8
sorrymother <notuntilsleep@gmail.com>
2023-06-05 00:05:52 +0300

    Update ViewController.swift

--- RestaurantsApp/ViewController.swift
+++ RestaurantsApp/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //ебала все!!!!!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
