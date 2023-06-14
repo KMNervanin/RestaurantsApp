42b07b3960ffd5274e93d92f6037728887454613
sorrymother <notuntilsleep@gmail.com>
2023-06-14 19:00:01 +0300

    pohui

--- RestaurantsApp/ViewController.swift
+++ RestaurantsApp/ViewController.swift
@@ -7,24 +7,25 @@
 
 import UIKit
 
-class ViewController: UITableViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     let pizzasNames = [
         "Pizza1", "Pizza2", "Pizza3", "Pizza4", "Pizza5", "Pizza6", "Pizza7", "Pizza8", "Pizza9", "Pizza10", "Pizza11", "Pizza12", "Pizza13", "Pizza14", "Pizza15", "Pizza16", "Pizza17", "Pizza18", "Pizza19", "Pizza20", "Pizza21", "Pizza22", "Pizza23", "Pizza24", "Pizza25", "Pizza26", "Pizza27", "Pizza28", "Pizza29", "Pizza30"
     ]
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return pizzasNames.count
     }
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Pizza")
-        cell?.textLabel?.text = "Pizza"
-        cell?.textLabel?.text = pizzasNames[indexPath.row]
-        return cell!
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let ident = "Pizza"
+        var cell: UITableViewCell
+        cell = tableView.dequeueReusableCell(withIdentifier: ident)!
+        cell.textLabel?.text = pizzasNames[indexPath.row]
+        return cell
     }
 }
 
