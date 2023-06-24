e428db3e4d07dd1c4ec425be293a0fcbd83a5749
sorrymother <notuntilsleep@gmail.com>
2023-06-24 23:36:43 +0300

    fix merge request comments
    deleted label at 17
    deleted frame & style tableView at 36
    created constraints(layout) with snapKit at 43-46
    changed options cell at 54.

--- RestaurantsApp/ViewController.swift
+++ RestaurantsApp/ViewController.swift
@@ -14,8 +14,6 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         "Pizza1", "Pizza2", "Pizza3", "Pizza4", "Pizza5", "Pizza6", "Pizza7", "Pizza8", "Pizza9", "Pizza10", "Pizza11", "Pizza12", "Pizza13", "Pizza14", "Pizza15", "Pizza16", "Pizza17", "Pizza18", "Pizza19", "Pizza20", "Pizza21", "Pizza22", "Pizza23", "Pizza24", "Pizza25", "Pizza26", "Pizza27", "Pizza28", "Pizza29", "Pizza30"
     ]
     
-    let label = UILabel()
-    
     var tableView = UITableView()
     
     let identifire = "Pizza"
@@ -35,13 +33,17 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     
     func createTable() {
-        self.tableView = UITableView(frame: view.bounds, style: .plain)
+        
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: identifire)
         self.tableView.delegate = self
         self.tableView.dataSource = self
         
         view.addSubview(tableView)
         
+        tableView.snp.makeConstraints {
+            $0.left.right.equalToSuperview().inset(0)
+            $0.top.bottom.equalToSuperview().inset(0)
+        }
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -49,9 +51,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let ident = "Pizza"
-        var cell: UITableViewCell
-        cell = tableView.dequeueReusableCell(withIdentifier: ident)!
+       let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: identifire, for:indexPath)
         cell.textLabel?.text = pizzasNames[indexPath.row]
         return cell
     }
