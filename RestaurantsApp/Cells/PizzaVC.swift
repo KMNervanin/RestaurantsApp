e61a85c1e40e2f17e06c114051101e0a4c773815
sorrymother <notuntilsleep@gmail.com>
2023-07-11 15:47:07 +0300

    newVCtest

---
+++ RestaurantsApp/Cells/PizzaVC.swift
@@ -0,0 +1,16 @@
+//
+//  PizzaVC.swift
+//  RestaurantsApp
+//
+//  Created by Дария Синицына on 11.07.2023.
+//
+
+import UIKit
+
+class PizzaVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
