fc080ac36d72995b92c3f59897b372ce90b30a25
sorrymother <notuntilsleep@gmail.com>
2023-09-07 18:58:01 +0300

    helpCollectionView

--- RestaurantsApp/Cells/PizzaVC.swift
+++ RestaurantsApp/Cells/PizzaVC.swift
@@ -7,12 +7,32 @@
 
 import UIKit
 
-class PizzaVC: UIViewController {
-
+class PizzaVC: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    
+    @IBOutlet weak var collectionview: UICollectionView!
+    
     var navTitle = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        collectionview.backgroundColor = .green
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collViewCell", for: indexPath)
+    }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        collectionview.visibleCells
     }
 
     override func viewWillAppear(_ animated: Bool) {
